9c2a487a83a88316ab864559b90463880752e3dc
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-06-15 13:27:15 +0700

    fix

--- src/components/ContentList.tsx
+++ src/components/ContentList.tsx
@@ -7,11 +7,11 @@ const ContentList = () => {
 
   if (isLoading) return <Loading />
 
-  if (error) return <p className="text-center text-red-500">{error}</p>
+  if (error || !contentList) return <p className="text-center text-red-500">{error}</p>
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 items-stretch justify-stretch mx-32 my-9 h-auto">
-      {contentList?.map((content) => (
+      {contentList.map((content) => (
         <ContentCard key={content.id} content={content} />
       ))}
     </div>
